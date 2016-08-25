b2198ecda1b199902671021ced19cbdfbefe4f2d
Bryan Harned <bbharned@gmail.com>
2016-08-25 16:47:33 -0400

    created partial for users as well as user edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ def new
     @user = User.new
 end
 
+
 def create
     @user = User.new(user_params)
     if @user.save
@@ -11,6 +12,22 @@ def create
         redirect_to events_path
     else
        render 'new'
+    end
+end
+
+
+def edit
+    @user = User.find(params[:id])
+end
+
+
+def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+        flash[:success] = "Your account has been updated"
+        redirect_to events_path
+    else
+        render 'edit'
     end
 end
 
