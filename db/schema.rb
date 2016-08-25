d56e25e09fbc861d483e64746a2c195463e5b976
Bryan Harned <bbharned@gmail.com>
2016-08-25 14:31:42 -0400

    added migrations maybe

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160825163002) do
+ActiveRecord::Schema.define(version: 20160825180457) do
 
   create_table "events", force: :cascade do |t|
     t.string   "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20160825163002) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "capacity"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
