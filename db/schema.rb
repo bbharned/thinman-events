8fc89d2b19e454e256d94bf1cd18634bd0077f27
Bryan Harned <bbharned@gmail.com>
2016-08-25 15:16:51 -0400

    added secure password and attribute

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160825180457) do
+ActiveRecord::Schema.define(version: 20160825191116) do
 
   create_table "events", force: :cascade do |t|
     t.string   "title"
@@ -29,8 +29,9 @@ ActiveRecord::Schema.define(version: 20160825180457) do
     t.string   "email"
     t.string   "company"
     t.string   "relation"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end
