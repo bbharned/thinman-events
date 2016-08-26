5a4c7c013a73ce17a668e56460dd8e3354880bbe
Bryan Harned <bbharned@gmail.com>
2016-08-26 11:01:59 -0400

    user show page and event partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,12 @@
 module ApplicationHelper
     
+    def gravitar_for(user, options = { size: 80 })
+        gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+        size = options[:size]
+        gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+        image_tag(gravatar_url, alt: user.firstname, class: "img-circle")
+    end
+
     # module ActionView
     #   module Helpers
     #     class FormBuilder 
