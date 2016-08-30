a0752b2c018af3e4e2dfee9f0ba0444ac56e243a
Bryan Harned <bbharned@gmail.com>
2016-08-30 13:35:14 -0400

    added many to many association table EventCategories

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,7 @@
 class Event < ActiveRecord::Base
     belongs_to :user
+    has_many :event_categories
+    has_many :categories, through: :event_categories
     validates :title, presence: true, length: { minimum:3, maximum: 100 }
     validates :description, presence: true, length: { minimum:10, maximum: 500 }
     validates :user_id, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160830124953) do
+ActiveRecord::Schema.define(version: 20160830171753) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "event_categories", force: :cascade do |t|
+    t.integer "event_id"
+    t.integer "category_id"
   end
 
   create_table "events", force: :cascade do |t|
