c7d73c09ef64d0a16e09aa6b3286aeb5753ad244
Bryan Harned <bbharned@gmail.com>
2016-09-06 17:49:52 -0400

    added additional validation for users already registered for events or not logged in

--- app/controllers/user_registers_controller.rb
+++ app/controllers/user_registers_controller.rb
@@ -9,6 +9,7 @@ class UserRegistersController < ApplicationController
       @registers = UserRegister.all
       @event = Event.find(params[:event_id])
       @register = UserRegister.new(user_id: current_user.id, event_id: params[:event_id])
+      @all_registered = @registers.where(event_id: @event.id)
       #@schedule =  UserRegister.where(event_id: @event.id, user_id: current_user.id)
 
       if @registers.any?{ |session| session.user_id == current_user.id and session.event_id == @event.id }
@@ -16,7 +17,13 @@ class UserRegistersController < ApplicationController
         flash[:danger] = "You Are Already Registered for This Event"
         redirect_to root_path
 
+      elsif @all_registered.count >= @event.capacity
+
+        flash[:danger] = "Sorry, registration for this event is full"
+        redirect_to events_path   
+
       else
+          
 
             if @register.save 
                   flash[:success] = "The selected event has been added to your schedule"
