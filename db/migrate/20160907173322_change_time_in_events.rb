410aa7906d5f57daee5256c4be4de8b0cf4e50ea
Bryan Harned <bbharned@gmail.com>
2016-09-07 15:20:05 -0400

    cleaning up

--- db/migrate/20160907173322_change_time_in_events.rb
+++
@@ -1,5 +0,0 @@
-class ChangeTimeInEvents < ActiveRecord::Migration[5.0]
-  def change
-    change_column :events, :time, :timestamp 
-  end
-end
--- db/migrate/20160907175045_change_time_column_again_in_events.rb
+++
@@ -1,5 +0,0 @@
-class ChangeTimeColumnAgainInEvents < ActiveRecord::Migration[5.0]
-  def change
-    change_column :events, :time, :timestamptz
-  end
-end
